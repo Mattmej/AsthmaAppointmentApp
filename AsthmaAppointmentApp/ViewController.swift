a355b6a523ae462a4b2a87e415ed834a4fb3071c
Matt Mejia <matt@ADMINs-MacBook-Pro.local>
2018-11-29 10:07:13 -0500

    Properly added header view to view controller. Will now work on table.

--- AsthmaAppointmentApp/ViewController.swift
+++ AsthmaAppointmentApp/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        topLabel.backgroundColor = UIColor(patternImage: UIImage(named: "adventure-alps-background-547114")!)
     }
 
     override func didReceiveMemoryWarning() {
