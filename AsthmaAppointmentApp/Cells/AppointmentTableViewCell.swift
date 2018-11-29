c0a163c12592f283787236f53f83c8031e8c6e26
Matt Mejia <matt@ADMINs-MacBook-Pro.local>
2018-11-29 10:24:28 -0500

    Add all view elements, and created groups for cells and models.

---
+++ AsthmaAppointmentApp/Cells/AppointmentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AppointmentTableViewCell.swift
+//  AsthmaAppointmentApp
+//
+//  Created by Matt Mejia on 11/29/18.
+//  Copyright © 2018 Matt_Mejia. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AsthmaAppointmentApp/Models/Header.swift
@@ -0,0 +1,9 @@
+//
+//  Header.swift
+//  AsthmaAppointmentApp
+//
+//  Created by Matt Mejia on 11/29/18.
+//  Copyright © 2018 Matt_Mejia. All rights reserved.
+//
+
+import Foundation
